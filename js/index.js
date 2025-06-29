2d883210a64df0085a5b6a65c7dba70066ce1f4d
adamkors29 <adamkors29@gmail.com>
2025-06-29 18:56:44 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ let done = false;
 $("div div i").on("click", function () {
     if (!done) {
         $("div h1").text("Люблю тебя!");
-        $("div").css("font-size", "40px");
+        $("div h1").css("font-size", "40px");
         $(this).animate({fontSize: "250px"});
     }
 });
